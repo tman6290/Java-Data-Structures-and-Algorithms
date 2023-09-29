37d0c4d350e25ae30a9686f13250a0d0d042cb25
tman6290 <tofaratifarinu@gmail.com>
2023-09-29 13:36:18 +0100

    I have solved practice questions about ArrayLists.

--- src/LearnArrayList.java
+++ src/LearnArrayList.java
@@ -2,8 +2,34 @@ import java.util.*;
 public class LearnArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		
+		//Q1. Add numbers 1 to 5 using a loop.
+		for (int i = 1; i < 6; i++) {
+			
+			list.add(i);
+		}
+		
+		//Q2. Print elements of the ArrayList.
+		System.out.println(list);
+		
+		//Q3. Remove the 4th element of the ArrayList.
+		list.remove(3);
+		
+		//Q4. Print all elements of the ArrayList.
+		System.out.println(list);
+		
+		//Q5. Print elements of the ArrayList one by one.
+		for (int num : list) {
+			
+			System.out.println(num);
+			
+		}
+		
 
 	}
+	
+	
 
 }
