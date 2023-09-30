22d6ef2931f3c76188ce0f71d9608d3f54dc4c6f
tman6290 <tofaratifarinu@gmail.com>
2023-09-30 12:14:55 +0100

    Just solved questions on HashSets

--- src/LearnHashSet.java
+++ src/LearnHashSet.java
@@ -3,6 +3,18 @@ public class LearnHashSet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Set <String> mySet = new HashSet<String>();
+		mySet.add("Happy");
+		mySet.add("New");
+		mySet.add("Year");
+		mySet.add("2021");
+		Iterator<String> iter = mySet.iterator();
+		while (iter.hasNext()) {
+			
+			System.out.println(iter.next());
+		}
+		
 
 	}
 
