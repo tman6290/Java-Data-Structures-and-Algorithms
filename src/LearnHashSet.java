bd1e229418356a52e20a0068d9a1bcc9209342b4
tman6290 <tofaratifarinu@gmail.com>
2023-10-03 20:03:08 +0100

    Finally fixed a bug with the LearnHashSet.Java file.

--- src/LearnHashSet.java
+++ src/LearnHashSet.java
@@ -5,15 +5,33 @@ public class LearnHashSet {
 		// TODO Auto-generated method stub
 		
 		Set <String> mySet = new HashSet<String>();
+		// Q1. Add the following elements: "Happy", "New", "Year", "2021".
 		mySet.add("Happy");
 		mySet.add("New");
 		mySet.add("Year");
 		mySet.add("2021");
+		
+		// Q2. Create an iterator of the HashSet.
 		Iterator<String> iter = mySet.iterator();
-		while (iter.hasNext()) {
+		
+		// Q3. Print the elements of the HashSet one by one.
+		while(iter.hasNext()) {
 			
 			System.out.println(iter.next());
 		}
+		
+        
+		// Q4. Print the elements of the HashSet one by one.
+		Iterator<String> iter2 = mySet.iterator();
+		while(iter2.hasNext()) {
+			
+			String element = iter2.next();
+			System.out.println(element.hashCode());
+		}
+		
+        
+		
+		
 		
 
 	}
--- src/LearnTreeSet.java
+++ src/LearnTreeSet.java
@@ -3,6 +3,8 @@ public class LearnTreeSet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Set <>
 
 	}
 
