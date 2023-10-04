655c60431a6c297fc276ad0ff4fabbc2875a9e76
tman6290 <tofaratifarinu@gmail.com>
2023-10-04 12:10:05 +0100

    Just finished solving problems on TreeSets.

--- src/LearnTreeSet.java
+++ src/LearnTreeSet.java
@@ -4,7 +4,21 @@ public class LearnTreeSet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Set <>
+		// Q1. Add the following elements:“Happy”, “New”, “Year”, “2021”.
+		Set<String> mySet = new TreeSet<String>();
+		mySet.add("Happy");
+		mySet.add("New");
+		mySet.add("Year");
+		mySet.add("Happy");
+		
+		// Q2. Create an iterator of the HashSet.
+		Iterator <String> iter = mySet.iterator();
+		
+		while(iter.hasNext()) {
+			
+			System.out.println(iter.next());
+			
+		}
 
 	}
 
